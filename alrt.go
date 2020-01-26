ce4cd490e80f3bca39805242525ba1b7b37c540d
Mike Hughes <intermernet@gmail.com>
2020-01-26 18:57:19 +1100

    Add GUI

--- alrt.go
+++ alrt.go
@@ -11,15 +11,16 @@ import (
 	"regexp"
 	"strings"
 	"time"
+
+	"fyne.io/fyne/app"
 )
 
 func randomTempo(min, max int) int {
-	if min > max {
-		fmt.Printf("Minimum BPM specified (%d) is greater than Maximum BPM (%d). Swapping them!\n", min, max)
-		min, max = max, min
+	tempo := min
+	if min != max {
+		rand.Seed(time.Now().UnixNano())
+		tempo = rand.Intn(max-min) + min
 	}
-	rand.Seed(time.Now().UnixNano())
-	tempo := rand.Intn(max-min) + min
 	fmt.Printf("Setting new tempo to %d\n", tempo)
 	return tempo
 }
@@ -31,12 +32,12 @@ func updateTempo(tempo int, data []byte) []byte {
 	lomVal := data[tIndices[2]:tIndices[3]]
 	repl := fmt.Sprintf("<Tempo>\n\t\t\t\t\t\t<LomId Value=\"%s\" />\n\t\t\t\t\t\t<Manual Value=\"%d\" />", string(lomVal), tempo)
 	data = tempoRe.ReplaceAllLiteral(data, []byte(repl))
-	aEventRe := regexp.MustCompile(`<AutomationEnvelope Id="1">\s+<EnvelopeTarget>\s+<PointeeId Value="(\d+)" />\s+</EnvelopeTarget>\s+<Automation>\s+<Events>\s+<FloatEvent Id="(\d+)" Time="(-*\d+)" Value="(\d+)" />`)
-	aVals := aEventRe.FindAllSubmatchIndex(data, -1)
+	aEnvRe := regexp.MustCompile(`<AutomationEnvelope Id="1">\s+<EnvelopeTarget>\s+<PointeeId Value="(\d+)" />\s+</EnvelopeTarget>\s+<Automation>\s+<Events>\s+<FloatEvent Id="(\d+)" Time="(-*\d+)" Value="(\d+)" />`)
+	aVals := aEnvRe.FindAllSubmatchIndex(data, -1)
 	aIndices := aVals[0]
 	aPointeeIDVal, aFEventID, aTime := data[aIndices[2]:aIndices[3]], data[aIndices[4]:aIndices[5]], data[aIndices[6]:aIndices[7]]
 	repl = fmt.Sprintf("<AutomationEnvelope Id=\"1\">\n\t\t\t\t\t\t<EnvelopeTarget>\n\t\t\t\t\t\t\t<PointeeId Value=\"%s\" />\n\t\t\t\t\t\t</EnvelopeTarget>\n\t\t\t\t\t\t<Automation>\n\t\t\t\t\t\t\t<Events>\n\t\t\t\t\t\t\t\t<FloatEvent Id=\"%s\" Time=\"%s\" Value=\"%d\" />", string(aPointeeIDVal), string(aFEventID), string(aTime), tempo)
-	data = aEventRe.ReplaceAllLiteral(data, []byte(repl))
+	data = aEnvRe.ReplaceAllLiteral(data, []byte(repl))
 	return data
 }
 
@@ -62,10 +63,11 @@ func main() {
 		os.Exit(1)
 	}
 	latest := vers[len(vers)-1]
+	cli := flag.Bool("cli", false, "Run CLI Version")
 	ver := flag.String("version", latest, "Version of Ableton Live to target. Defaults to latest version.\nUSAGE alrt -version \""+latest+"\"")
 	listVer := flag.Bool("list", false, "List installed versions of Ableton Live")
-	min := flag.Int("min", 120, "The minimum BPM")
-	max := flag.Int("max", 120, "The maximum BPM")
+	min := flag.Int("min", 110, "The minimum BPM")
+	max := flag.Int("max", 130, "The maximum BPM")
 	flag.Parse()
 	if *listVer {
 		fmt.Println("Installed versions of Ableton Live:")
@@ -77,6 +79,10 @@ func main() {
 	if *min < 20 || *min > 999 || *max < 20 || *max > 999 {
 		fmt.Println(fmt.Errorf("invalid BPM Specified \"-min\" and \"-max\" must be between 20 and 999 inclusive"))
 		os.Exit(1)
+	}
+	if *min > *max {
+		fmt.Printf("Minimum BPM specified (%d) is greater than Maximum BPM (%d). Swapping them!\n", *min, *max)
+		min, max = max, min
 	}
 	templLoc := getDefaultTemplate(*ver)
 	templZip, err := os.OpenFile(templLoc, os.O_RDWR, 0755)
@@ -116,7 +122,17 @@ func main() {
 		fmt.Println(fmt.Errorf("Could not close gzip reader: %v", err))
 		os.Exit(1)
 	}
-	out := updateTempo(randomTempo(*min, *max), data)
+
+	var rt int
+	switch {
+	case *cli:
+		rt = randomTempo(*min, *max)
+	default:
+		a := app.New()
+		rt = runGUI(a, *min, *max)
+	}
+
+	out := updateTempo(rt, data)
 	err = templZip.Truncate(0)
 	if err != nil {
 		fmt.Println(fmt.Errorf("Could not truncate file %s: %v", templLoc, err))
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Intermernet/alrt
 
 go 1.13
+
+require (
+	fyne.io/fyne v1.2.1
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+)
